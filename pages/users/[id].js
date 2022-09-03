d8e310391aa20634c1841050ad1fc940dcb70b8d
gaev-art <forsakensr@gmail.com>
2022-09-03 14:38:47 +0300

    added getting user from server

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,21 +1,19 @@
-import {useRouter} from "next/router";
-
 export default function User ({user}) {
-  const {query} = useRouter();
   return (
     <>
-      <h1>user with id {query.id}</h1>
-      <div>{user.name}</div>
+      {user.statusCode
+        ? <h1>{user.message}</h1>
+        :<h1>{user.name}</h1>}
     </>
   );
 };
 
 
 export async function getServerSideProps({params}) {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/users/${params.id}`);
   const user = await response.json();
 
   return {
-    props: {user}, // will be passed to the page component as props
+    props: {user},
   }
 }
